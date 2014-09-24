143cad0d2ab3c7e325624899789566ee1e17801b
HermannBjorgvin <hermann3646@gmail.com>
2014-09-24 02:01:51 +0000

    Take off
    Something to play with, Bounding box still needs work along with the
    airplane controls

--- js/game.js
+++ js/game.js
@@ -1,6 +1,18 @@
 
+/**************
+	GLOBALS
+**************/
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var TO_RADIANS = Math.PI/180;
+
+var imageObj = new Image();
+imageObj.src = 'assets/cursor.png';
+
+/****************
+	GAME TICK
+****************/
 
 gameTick.run(tick);
 
@@ -16,44 +28,125 @@ function tick(elapsed){
 
 }
 
+
 // This is my aircraft, there are many like it but this one is mine
 var aircraft = {
 	name: "TF-DESTROYER",
-	x: 20,
-	y: 20,
+	x: 300,
+	y: 200,
 	velX: 0,
 	velY: 0,
-	speed: 100,
-	engineOn : false,
+	speed: 20,
+	drag: 0.4,
+	direction: 0,
 
-	turnCW: false, // booleans that controll rotation
+	engineOn: false, // booleans that control movement
+	turnCW: false,
 	turnCCW: false,
 
 	draw: function(){
-		ctx.fillStyle = '#000';
-		ctx.fillRect(this.x, this.y, 20, 20);
+		drawRotatedImage(imageObj, this.x, this.y, this.direction);
 	},
 	move: function(elapsed){
+
+		// Gravity
 		this.velY += 9.81 * elapsed;
 
-		this.y += this.velY;
+		// Horizontal air resistance
+		this.velX -= this.velX * (this.drag * elapsed);
+
+		// Change direction
+		if (this.turnCW) {
+			this.direction += 360 * elapsed;
+			this.direction = Math.abs(this.direction % 360);
+		};
+		if (this.turnCCW) {
+			this.direction -= 360 * elapsed;
+			this.direction = Math.abs(this.direction % 360);
+		};
+		//console.log(this.direction);
+
+		// Add engine velocity
+		if (this.engineOn) {
+			var angle = this.direction;
+
+			var x = Math.cos(angle);
+			var y = Math.sin(angle);
+
+			this.velX += this.speed * x * elapsed;
+			this.velY += this.speed * y * elapsed;
+		};
+
+		// Bounding box - Got to find a better bounding box
+		if (this.y >= canvas.height) {
+			this.y = canvas.height-1;
+			this.velY = -this.velY * 0.75;
+		};
+		if (this.x >= canvas.width) {
+			this.x = 0;
+		};
+		if (this.x < 0) {
+			this.x = canvas.width;
+		};
+
+		this.y = Math.min(this.y + this.velY, canvas.height);
+		this.x = Math.min(this.x + this.velX, canvas.width);
 	},
-	turn
+	keyHandler: function(key, state){
+		switch(key){
+			case 'w':
+				this.engineOn = state;
+			break;
+			case 'a':
+				this.turnCCW = state;
+			break;
+			case 'd':
+				this.turnCW = state;
+			break;
+		}
+	}
 };
 
 /***********************
-	AIRCRAFT CONTROL
+	VARIOUS FUNCTIONS
 ***********************/
+
+function drawRotatedImage(image, x, y, angle)
+{ 
+    // save the current co-ordinate system 
+    // before we screw with it
+    ctx.save(); 
+
+    // move to the middle of where we want to draw our image
+    ctx.translate(x, y);
+
+    // rotate around that point, converting our 
+    // angle from degrees to radians 
+    ctx.rotate(angle * TO_RADIANS);
+
+    // draw it up and to the left by half the width
+    // and height of the image 
+    ctx.drawImage(image, -(image.width/2), -(image.height/2));
+
+    // and restore the co-ords to how they were when we began
+    ctx.restore(); 
+}
+
+/***********************
+		CONTROLS
+***********************/
+
 Mousetrap.bind({
-    'a': function() { console.log('a DOWN'); },
-    'd': function() { console.log('d DOWN'); }
+    'a': function() { aircraft.keyHandler('a', true); },
+    'd': function() { aircraft.keyHandler('d', true); },
+    'w': function() { aircraft.keyHandler('w', true); }
 }, 'keydown');
 
 Mousetrap.bind({
-    'a': function() { console.log('a UP'); },
-    'd': function() { console.log('d UP'); }
+    'a': function() { aircraft.keyHandler('a', false); },
+    'd': function() { aircraft.keyHandler('d', false); },
+    'w': function() { aircraft.keyHandler('w', false); }
 }, 'keyup');
-
 
 /***********************
 	AIRCRAFT UPDATES

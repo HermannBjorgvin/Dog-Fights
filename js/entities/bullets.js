09447fb681f7c4d253bd3072f93638780d47bf70
HermannBjorgvin <hermann3646@gmail.com>
2014-09-28 05:46:36 +0000

    BULLETS!
    FÖNN FÖNN FÖNN

---
+++ js/entities/bullets.js
@@ -0,0 +1,63 @@
+
+// This is my rifle, there are many like it but this one is mine
+function bulletConstructor(x_coords, y_coords, x_vel, y_vel, direction){
+	this.active = 1;
+	this.x = x_coords; // Horizontal position
+	this.y = y_coords; // Vertical position
+	this.velX = x_vel; // Horizontal velocity
+	this.velY = y_vel; // Vertical velocity
+	this.power = 10; // Max engine output
+	this.drag = 0.1; // Drag coefficient
+	this.direction = direction; // 360° direction
+	this.radius = 4;
+	this.age = 0;
+	this.maxAge = 1;
+	this.initialized = 0;
+
+	this.draw = function(){
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+		ctx.fillStyle = '#000';
+		ctx.fill();
+	};
+	this.iterate = function(elapsed){
+
+		var gravity = {
+			earth:9.81,
+			mars:3.711,
+			moon:1.622
+		};
+
+		// Velocity calculation
+		var angle = this.direction % 360;
+		var x = Math.cos(angle * TO_RADIANS);
+		var y = Math.sin(angle * TO_RADIANS);
+		this.velX += this.power * x * elapsed;
+		this.velY += this.power * y * elapsed;
+
+		if (this.initialized != 1) {
+			this.velX += this.power * x;
+			this.velY += this.power * y;
+			this.initialized = 1;
+		};
+
+		// Bounding box - Got to find a better bounding box		
+		if (this.x >= canvas.width) {
+			this.x = 0;
+		};
+		if (this.x < 0) {
+			this.x = canvas.width;
+		};
+		if (this.y > canvas.height-gameSettings.groundHeight) {
+			this.active = 0;
+		};
+
+		this.y = this.y + this.velY;
+		this.x = this.x + this.velX;
+
+		this.age += elapsed;
+		if (this.age >= this.maxAge) {
+			this.active = 0;
+		};
+	};
+};
--- js/game.js
+++ js/game.js
@@ -7,58 +7,89 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var TO_RADIANS = Math.PI/180;
 
+var bullets = [];
+
 var planeSpritesheet = new Image();
 planeSpritesheet.src = 'assets/plane.png';
 
 var groundTexture = new Image();
 groundTexture.src = 'assets/groundTexture.png';
-var groundPattern=ctx.createPattern(groundTexture,"repeat");
+var groundPattern;
 
-var bullets = [];
+groundTexture.onload = function(){
+	groundPattern = ctx.createPattern(groundTexture,"repeat");
+}
+
 
 /****************
 	GAME TICK
 ****************/
 
-gameTick.run(tick);
+window.onload = function(){
+
+	gameTick.run(tick);
+
+}
 
 function tick(elapsed){
 
-	// Location/Velocity/AI
-	aircraft.fly(elapsed);
-
-	// Drawings
+	// Clear canvas
 	clearCanvas();
-	draw();
+
+	// Draw game background and clouds and shit
+	drawBackground();
+
+	// Location/Velocity/AI
+	aircraft.iterate(elapsed);
+	aircraft.draw();
+
+	// Bullets
+	for (var i = bullets.length - 1; i >= 0; i--) {
+		bullets[i].iterate(elapsed);
+		bullets[i].draw();
+		if (bullets[i].active != 1) {
+			bullets.splice(i,1);
+		};
+	};
 
 }
 
 var gameSettings = {
-	groundHeight: 60
+	groundHeight: 60,
+	gravity: {
+		earth:9.81 * 0.4,
+		mars:3.711 * 0.4,
+		moon:1.622 * 0.4
+	}
 }
 
 // This is my aircraft, there are many like it but this one is mine
 var aircraft = {
 	name: "TF-DESTROYER",
 	x: 300, // Horizontal position
-	y: 200, // Vertical position
+	y: 530, // Vertical position
 	velX: 0, // Horizontal velocity
 	velY: 0, // Vertical velocity
 	
-	power: 16, // Max engine output
-	drag: 0.3, // Drag coefficient
+	width: (68 * 0.5),
+	height: (87 * 0.5),
+	scale: 0.5,
+	power: 6, // Max engine output
+	drag: 0.2, // Drag coefficient
 	lift: 1.2, // Lift coefficient
 	direction: 0, // 360° direction
 
 	engineOn: false, // booleans that control movement
 	turnCW: false, // Is turning clockwise
 	turnCCW: false, // Is turning counter clockwise
-	primaryWeapon: false, // Primary weapon 
+	primaryWeapon: false, // Primary weapon
+	primaryWeaponChargeTime: 0.125,
+	primaryWeaponCharge:0,
 
 	draw: function(){
 		//drawRotatedImage(planeSpritesheet, this.x, this.y, this.direction, 0.5);
 
-		var scale = 0.5;
+		var scale = this.scale;
 		var angle = this.direction % 360;
 		var x = Math.cos(angle * TO_RADIANS);
 		var y = Math.sin(angle * TO_RADIANS);
@@ -67,7 +98,7 @@ var aircraft = {
 		var frame = Math.round(Math.abs(x * (frameNumber-1)));
 
 		var img = planeSpritesheet;
-		var frameWidth = 68;
+		var frameWidth = img.width/frameNumber;
 		var frameHeight = img.height;
 
 		ctx.save();
@@ -79,18 +110,12 @@ var aircraft = {
 		ctx.restore();
 
 	},
-	fly: function(elapsed){
+	iterate: function(elapsed){
 		
 		var angle = this.direction % 360;
 
-		var gravity = {
-			earth:9.81,
-			mars:3.711,
-			moon:1.622
-		};
-
 		// Gravity
-		this.velY += gravity.earth * 0.5 * elapsed;
+		this.velY += gameSettings.gravity.earth * 0.5 * elapsed;
 
 		// Drag coefficient
 		this.velX = bringToZero(this.velX, (Math.pow(this.velX, 2) * this.drag * elapsed));
@@ -127,28 +152,42 @@ var aircraft = {
 		/***************************
 				BOUNDING BOX
 		***************************/
-			// RIGHT (exit right enter left)
-			if (this.x >= canvas.width) {
-				this.x = 0;
-			};
-			// LEFT (exit left enter right)
-			if (this.x < 0) {
-				this.x = canvas.width;
-			};
-			// TOP (ease down)
-			if (this.y < 0) {
-				this.velY += Math.pow(this.y, 2) * elapsed;
-			};
-			// BOTTOM (increased friction at ground level)
-			if (this.y > canvas.height-gameSettings.groundHeight) {
-				this.y = canvas.height-gameSettings.groundHeight;
-				this.velX = bringToZero(this.velX, 3 * elapsed);
-				this.velY = bringToZero(this.velY, 2 * elapsed);
-				this.velY = -this.velY*0.6;
-			};
+		// RIGHT (exit right enter left)
+		if (this.x >= canvas.width) {
+			this.x = 0;
+		};
+		// LEFT (exit left enter right)
+		if (this.x < 0) {
+			this.x = canvas.width;
+		};
+		// TOP (ease down)
+		if (this.y < 0) {
+			this.velY += Math.pow(this.y, 2) * elapsed;
+		};
+		// BOTTOM (increased friction at ground level)
+		if (this.y > canvas.height-gameSettings.groundHeight) {
+			this.y = canvas.height-gameSettings.groundHeight;
+			this.velX = bringToZero(this.velX, 3 * elapsed);
+			this.velY = bringToZero(this.velY, 2 * elapsed);
+			this.velY = -this.velY*0.6;
+		};
 
-		this.y = Math.min(this.y + this.velY, canvas.height);
-		this.x = Math.min(this.x + this.velX, canvas.width);
+		this.y = this.y + this.velY;
+		this.x = this.x + this.velX;
+
+		/**********************
+				WEAPONS
+		**********************/
+
+		this.primaryWeaponCharge += elapsed;
+		if (this.primaryWeapon && this.primaryWeaponCharge >= this.primaryWeaponChargeTime) {
+			var x_delta = Math.cos(angle * TO_RADIANS) * this.width;
+			var y_delta = Math.sin(angle * TO_RADIANS) * this.width;
+
+			var newBullet = new bulletConstructor(this.x + x_delta, this.y + y_delta, this.velX, this.velY, this.direction);
+			bullets.push(newBullet);
+			this.primaryWeaponCharge = 0;
+		};
 	},
 	keyHandler: function(key, state){
 		switch(key){
@@ -162,73 +201,11 @@ var aircraft = {
 				this.turnCW = state;
 			break;
 			case 'primaryWeapon':
-				this.turnCW = state;
+				this.primaryWeapon = state;
 			break;
 		}
 	}
 };
-
-// This is my aircraft, there are many like it but this one is mine
-function bullet_constructor(){
-	x: 300; // Horizontal position
-	y: 200; // Vertical position
-	velX: 0; // Horizontal velocity
-	velY: 0; // Vertical velocity
-	power: 80; // Max engine output
-	drag: 0.0125; // Drag coefficient
-	direction: 0; // 360° direction
-
-	draw = function(){
-		drawRotatedImage(imageObj, this.x, this.y, this.direction, 0.5);
-	};
-	move = function(elapsed){
-
-		this.radius = 5;
-
-		// Drag coefficient
-		this.velX = bringToZero(this.velX, (Math.pow(this.velX, 2) * this.drag * elapsed));
-		this.velY = bringToZero(this.velY, (Math.pow(this.velY, 2) * this.drag * elapsed));
-
-		// Aircraft lift - simple variation, not accurate
-		this.velY -= this.velX * this.lift * elapsed;
-
-		// Change direction
-		if (this.turnCW) {
-			this.direction += 360 * elapsed * 0.5;
-		};
-		if (this.turnCCW) {
-			this.direction -= 360 * elapsed * 0.5;
-		};
-		//console.log(this.direction);
-
-		// Add engine velocity
-		if (this.engineOn) {
-			var angle = this.direction % 360;
-
-			var x = Math.cos(angle * TO_RADIANS);
-			var y = Math.sin(angle * TO_RADIANS);
-
-			this.velX += this.power * x * elapsed;
-			this.velY += this.power * y * elapsed;
-		};
-
-		// Bounding box - Got to find a better bounding box		
-		if (this.x >= canvas.width) {
-			this.x = 0;
-		};
-		if (this.x < 0) {
-			this.x = canvas.width;
-		};
-		if (this.y >= canvas.height-100) {
-			this.y = canvas.height-101;
-			this.velY = -this.velY * 0.75;
-		};
-
-		this.y = Math.min(this.y + this.velY, canvas.height);
-		this.x = Math.min(this.x + this.velX, canvas.width);
-	};
-};
-
 
 /***********************
 	HELPER FUNCTIONS
@@ -285,11 +262,6 @@ Mousetrap.bind({
 /***********************
 	RENDERING ENGINE	
 ***********************/
-
-function draw(){
-	drawBackground();
-	aircraft.draw();
-}
 
 function drawBackground(){
 
